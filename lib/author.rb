d52a3b66c20d77da9974ce2afa1e6964c97935ff
Diana Utan <didiutzi@gmail.com>
2021-02-16 20:05:31 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,9 +10,9 @@ class Author
   end
 
   def add_post(post)
-  @posts << post
-  post.author  = self
-  @@count +=1
+    @posts << post
+    post.author  = self
+    @@count +=1
   end
 
   def add_post_by_title(title)
